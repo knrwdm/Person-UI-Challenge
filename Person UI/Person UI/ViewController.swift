832a8fdf039dce32e337d4844224ea1d041de735
Kayla <knrwdm@mail.missouri.edu>
2019-09-12 00:04:51 -0500

    First Commit

---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Kayla Reap on 9/12/19.
+//  Copyright © 2019 Kayla Reap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
